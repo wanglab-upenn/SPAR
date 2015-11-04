34f9f34c2a2e6eb0a7484260ef03b4872ea9edde
Pavel Kuksa <pkuksa@upenn.edu>
2015-11-04 13:24:32 -0500

    updated trim_adapter.sh

--- scripts/trim_adapter.sh
+++ scripts/trim_adapter.sh
@@ -3,7 +3,17 @@
 set -e
 
 #source `dirname $0`/../config.sh
-CUTADAPT=${HOME}/bin/cutadapt-1.9/bin/cutadapt
+CUTADAPT=`which cutadapt 2> /dev/null | tail -n 1` || ${HOME}/bin/cutadapt-1.8.1/bin/cutadapt
+echo ${CUTADAPT}
+command -v ${CUTADAPT} > /dev/null 2>&1 || { echo >&2 "$0 requires cutadapt, but it is not found. Please cu"; exit 1; }
+
+if [ $# -lt 1 ]
+then
+  echo "USAGE: `basename $0` reads.fastq <-a ADAPTERSEQ3p> <-g ADAPTERSEQ5p>"
+  exit 1
+fi
+
+
 
 INFASTQ=$1
 ADAPTERoption=$@
@@ -26,10 +36,10 @@ UNTRIMMEDREADS=${DATASET}_untrimmed.fastq
 TOOSHORTREADS=${DATASET}_tooshort.fastq
 
 
-${CUTADAPT} ${ADAPTERoption} -e $MAX_ADAPTER_ERROR -o $TRIMMEDREADS \
---untrimmed-output $UNTRIMMEDREADS --too-short-output $TOOSHORTREADS \
--O $MIN_ADAPTER_MATCH -m $MIN_TRIMMED_READ_LEN -n 2 \
---length-tag "length=" $RAWREADS
+${CUTADAPT} ${ADAPTERoption} -e ${MAX_ADAPTER_ERROR} -o ${TRIMMEDREADS} \
+--untrimmed-output ${UNTRIMMEDREADS} --too-short-output ${TOOSHORTREADS} \
+-O ${MIN_ADAPTER_MATCH} -m ${MIN_TRIMMED_READ_LEN} -n 2 \
+--length-tag "length=" ${INFASTQ}
 
 
 #${CUTADAPT} -a ${ADAPTER3p} -g ${ADAPTER5p} -e $MAX_ADAPTER_ERROR -o $TRIMMEDREADS \
